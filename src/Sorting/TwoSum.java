01e5e242e50ac2829122375e3a92ec377d4f292a
thkhan009 <tazveerhossainkhan009@gmail.com>
2023-09-13 21:54:14 +0600

    1. Two Sum https://leetcode.com/problems/two-sum/ -> Using Hashmap Data structure

--- src/Sorting/TwoSum.java
+++ src/Sorting/TwoSum.java
@@ -1,5 +1,6 @@
 /*1. Two Sum
-https://leetcode.com/problems/two-sum/*/
+https://leetcode.com/problems/two-sum/*//*
+
 package Sorting;
 
 import java.util.Arrays;
@@ -22,3 +23,41 @@ public class TwoSum {
         return null;
     }
 }
+*/
+
+package Sorting;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> Map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int rem = target - nums[i];
+            if(Map.containsKey(rem)){
+                return new int[]{Map.get(rem),i};
+            }else {
+                Map.put(nums[i],i);
+            }
+        }
+        return null;
+    }
+}
+  /*  static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+    }*/
